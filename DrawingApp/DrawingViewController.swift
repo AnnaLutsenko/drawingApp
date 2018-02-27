64d2b61abfba07f445eab0e72ab0d6edb056b62c
Anna Lutsenko <nyuta.lutsenko@gmail.com>
2018-02-27 15:39:37 +0200

    added actions for done/undo buttons

--- DrawingApp/DrawingViewController.swift
+++ DrawingApp/DrawingViewController.swift
@@ -12,7 +12,11 @@ class DrawingViewController: UIViewController {
     
     @IBOutlet var colorsBtns: [DrawingColorButton]!
     @IBOutlet weak var drawView: DrawView!
+    @IBOutlet weak var sendBtn: SendButton!
     @IBOutlet weak var viewWithColors: UIView!
+    
+    @IBOutlet weak var viewUndoCancelBtn: UIView!
+    @IBOutlet weak var viewWithMainBtn: UIView!
     
     let colorsArr = ["FFFFFF", "000000", "228AE6", "16AABF", "41C057", "FAB005", "FD7E13", "FA5251", "FF43AD", "BE4ADB"]
 
@@ -32,7 +36,15 @@ class DrawingViewController: UIViewController {
         gradient.colors = [UIColor.black.withAlphaComponent(0).cgColor, UIColor.black.withAlphaComponent(1).cgColor]
         viewWithColors.layer.insertSublayer(gradient, at: 0)
     }
+    
+    func sendBtn(isHidden: Bool) {
+        sendBtn.isHidden = isHidden
+        viewWithMainBtn.isHidden = isHidden
+        viewWithColors.isHidden = !isHidden
+        viewUndoCancelBtn.isHidden = !isHidden
+    }
 
+    //  MARK: - Actions
     @IBAction func colorSelected(_ sender: UIButton) {
         colorsBtns.forEach { btn in btn.isSelected = false}
         sender.isSelected = true
@@ -40,5 +52,19 @@ class DrawingViewController: UIViewController {
         drawView.drawColor = selectedColor
     }
 
+    @IBAction func drawBtnClicked(_ sender: UIButton) {
+        viewWithColors.isHidden = false
+        sendBtn(isHidden: true)
+    }
+
+    @IBAction func undoBtnClicked(_ sender: UIButton) {
+        
+        drawView.layer.sublayers?.removeLast()
+        drawView.layer.layoutSublayers()
+    }
+    
+    @IBAction func doneBtnClicked(_ sender: UIButton) {
+        sendBtn(isHidden: false)
+    }
 }
 
--- DrawingApp/View/DrawView.swift
+++ DrawingApp/View/DrawView.swift
@@ -14,35 +14,18 @@ class DrawView: UIView {
     var drawColor = UIColor.black
     
     var path = UIBezierPath()
-    var pathArr: [UIBezierPath] = []
     var currentColor = UIColor.black
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        path.lineWidth = 5.0
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        if drawColor == currentColor {
-            lastPoint = touches.first?.location(in: self)
-            path.move(to: lastPoint)
-        } else {
-            
-            let a = CAShapeLayer()
-            a.path = path.cgPath
-            a.strokeColor = currentColor.cgColor
-            a.fillColor = nil
-            a.lineWidth = path.lineWidth
-            layer.addSublayer(a)
-//            layer.insertSublayer(a, at: 0)
-//            pathArr.append(path)
-            
-            path = UIBezierPath()
-            path.lineWidth = 5
-            lastPoint = touches.first?.location(in: self)
-            path.move(to: lastPoint)
-        }
+        path = UIBezierPath()
+        path.lineWidth = 5
+        lastPoint = touches.first?.location(in: self)
+        path.move(to: lastPoint)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -52,7 +35,16 @@ class DrawView: UIView {
             self.setNeedsDisplay()
         }
     }
-
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let a = CAShapeLayer()
+        a.path = path.cgPath
+        a.strokeColor = currentColor.cgColor
+        a.fillColor = nil
+        a.lineWidth = path.lineWidth
+        layer.addSublayer(a)
+    }
+    
     override func draw(_ rect: CGRect) {
         currentColor = drawColor
         drawColor.setStroke()
