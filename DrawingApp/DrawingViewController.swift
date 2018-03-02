6631abb52258cab602c7f6e3e98531fb904981b9
Anna Lutsenko <nyuta.lutsenko@gmail.com>
2018-03-02 16:30:17 +0200

    implemented adding shapes and text to drawView

--- DrawingApp/DrawingViewController.swift
+++ DrawingApp/DrawingViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class DrawingViewController: UIViewController {
     
+    let screenSize = UIScreen.main.bounds
     enum SelectedBtn: Int {
         case textBtn = 0, drawBtn, shapeBtn, imgBtn
     }
+    var selectedDrawBtn = SelectedBtn.drawBtn
     
     @IBOutlet var colorsBtns: [DrawingColorButton]!
     @IBOutlet weak var drawView: DrawView!
+    @IBOutlet weak var pencilBtn: UIButton!
     @IBOutlet weak var sendBtn: SendButton!
+    @IBOutlet weak var shapeStackView: UIStackView!
     @IBOutlet weak var viewWithColors: UIView!
     
     @IBOutlet weak var viewUndoCancelBtn: UIView!
     @IBOutlet weak var viewWithMainBtn: UIView!
     
     var imagePicker: UIImagePickerController!
+    var tempTextView: UITextView!
     
     let colorsArr = ["FFFFFF", "000000", "228AE6", "16AABF", "41C057", "FAB005", "FD7E13", "FA5251", "FF43AD", "BE4ADB"]
+    var selectedColor = UIColor.black
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +39,12 @@ class DrawingViewController: UIViewController {
     }
     
     func initController() {
-        viewWithColors.isHidden = true
         imagePicker =  UIImagePickerController()
         imagePicker.delegate = self
+        
+        let pencilImg = UIImage(named: "pencilIcon")
+        let tintedImage = pencilImg?.withRenderingMode(.alwaysTemplate)
+        pencilBtn.setImage(tintedImage, for: .normal)
     }
     
     func addGradientToView() {
@@ -48,9 +57,18 @@ class DrawingViewController: UIViewController {
     
     func sendBtn(isHidden: Bool) {
         sendBtn.isHidden = isHidden
+        sendBtn.isEnabled = false
         viewWithMainBtn.isHidden = isHidden
         viewWithColors.isHidden = !isHidden
         viewUndoCancelBtn.isHidden = !isHidden
+        sendBtnIsEnabled()
+    }
+    
+    func sendBtnIsEnabled() {
+        sendBtn.isEnabled = false
+        if let count = drawView.layer.sublayers?.count {
+            sendBtn.isEnabled = count > 0
+        }
     }
     
     func addImgAsLayer(_ img: UIImage) {
@@ -60,31 +78,114 @@ class DrawingViewController: UIViewController {
         imgLayer.frame = drawView.bounds
         imgLayer.contents = myImg
         drawView.layer.addSublayer(imgLayer)
+        sendBtnIsEnabled()
     }
-
+    
+    func addTextViewToView() {
+        tempTextView =  UITextView(frame: CGRect(x: 0, y: 300, width: screenSize.width, height: 500))
+        tempTextView.font = UIFont.boldSystemFont(ofSize: 22)
+        tempTextView.textColor = selectedColor
+        tempTextView.autocorrectionType = .no
+        tempTextView.textAlignment = .center
+        tempTextView.becomeFirstResponder()
+        view.addSubview(tempTextView)
+    }
+    
+    func createCALayerFromTextView() {
+        tempTextView.resignFirstResponder()
+        guard let text = tempTextView.text else { return }
+        
+        tempTextView.removeFromSuperview()
+        drawView.createLayerWith(text: text, height: tempTextView.getHeight(), width: tempTextView.getWidth())
+    }
+    
     //  MARK: - Actions
+    
+    @IBAction func addShape(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            drawView.createSquare(radius: 0)
+        case 1:
+            drawView.createSquare(radius: 50)
+        case 2:
+            drawView.createTriangle()
+        default:
+            return
+        }
+    }
+    
+//    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+//        let translation = recognizer.translation(in: self.view)
+//        if let view = recognizer.view {
+//            view.center = CGPoint(x:view.center.x + translation.x,
+//                                  y:view.center.y + translation.y)
+//        }
+//        recognizer.setTranslation(CGPoint.zero, in: self.view)
+//    }
+    
     @IBAction func colorSelected(_ sender: UIButton) {
         colorsBtns.forEach { btn in btn.isSelected = false}
         sender.isSelected = true
-        let selectedColor = UIColor.init(hex: colorsArr[sender.tag])
+        selectedColor = UIColor.init(hex: colorsArr[sender.tag])
+        pencilBtn.tintColor = selectedColor
         drawView.drawColor = selectedColor
+        
+        switch selectedDrawBtn {
+        case .textBtn:
+            tempTextView.textColor = selectedColor
+        default:
+            return
+        }
     }
     
     @IBAction func textBtnClicked(_ sender: UIButton) {
+        selectedDrawBtn = SelectedBtn.textBtn
         viewWithColors.isHidden = false
         sendBtn(isHidden: true)
+        
+        addTextViewToView()
     }
 
     @IBAction func drawBtnClicked(_ sender: UIButton) {
+        selectedDrawBtn = SelectedBtn.drawBtn
         viewWithColors.isHidden = false
         sendBtn(isHidden: true)
     }
     
     @IBAction func shapeBtnClicked(_ sender: UIButton) {
+        selectedDrawBtn = SelectedBtn.shapeBtn
         viewWithColors.isHidden = false
         sendBtn(isHidden: true)
+        
+        shapeStackView.isHidden = false
     }
-
+    
+    @IBAction func doneBtnClicked(_ sender: UIButton) {
+        sendBtn(isHidden: false)
+        sendBtnIsEnabled()
+        switch selectedDrawBtn {
+        case .shapeBtn:
+            shapeStackView.isHidden = true
+        case .textBtn:
+            createCALayerFromTextView()
+        default:
+            return
+        }
+    }
+    
+    @IBAction func undoBtnClicked(_ sender: UIButton) {
+        drawView.removeLastLayer()
+        sendBtnIsEnabled()
+    }
+    
+    @IBAction func sendBtnClicked(_ sender: UIButton) {
+        let renderer = UIGraphicsImageRenderer(size: drawView.bounds.size)
+        let image = renderer.image { ctx in
+            drawView.drawHierarchy(in: drawView.bounds, afterScreenUpdates: true)
+        }
+        print(image)
+    }
+    
     @IBAction func addImgBtnClicked(_ sender: UIButton) {
         let alertController = UIAlertController(title: nil, message: nil , preferredStyle: .actionSheet)
         
@@ -105,14 +206,6 @@ class DrawingViewController: UIViewController {
         alertController.addAction(cancelAction)
         
         present(alertController,animated: true, completion: nil)
-    }
-    
-    @IBAction func undoBtnClicked(_ sender: UIButton) {
-        drawView.removeLastLayer()
-    }
-    
-    @IBAction func doneBtnClicked(_ sender: UIButton) {
-        sendBtn(isHidden: false)
     }
 }
 
--- DrawingApp/View/DrawView.swift
+++ DrawingApp/View/DrawView.swift
@@ -57,4 +57,39 @@ class DrawView: UIView {
     func removeLastLayer() {
         layer.sublayers?.removeLast()
     }
+    
+    func createTriangle() {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: self.frame.width/2, y: self.frame.height/2))
+        path.addLine(to: CGPoint(x: self.frame.width/2-100, y: self.frame.size.height/2+150))
+        path.addLine(to: CGPoint(x: self.frame.width/2+100, y: self.frame.size.height/2+150))
+        path.close()
+        
+        let a = CAShapeLayer()
+        a.path = path.cgPath
+        a.fillColor = drawColor.cgColor
+        layer.addSublayer(a)
+    }
+    
+    func createSquare(radius: Int) {
+        let xCoord = CGFloat(self.frame.size.width/2-50)
+        let yCoord = CGFloat(self.frame.size.height/2-50)
+        
+        let path = UIBezierPath(roundedRect: CGRect(x: xCoord, y: yCoord, width: 100, height: 100), cornerRadius: CGFloat(radius))
+        
+        let a = CAShapeLayer()
+        a.path = path.cgPath
+        a.fillColor = drawColor.cgColor
+        layer.addSublayer(a)
+    }
+    
+    func createLayerWith(text: String, height: CGFloat, width: CGFloat) {
+        let a = CATextLayer()
+        a.alignmentMode = kCAAlignmentCenter
+        a.frame = CGRect(x: (bounds.width-width)/2, y: (bounds.height-height)/2, width: width, height: height)
+        let myAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 22), NSAttributedStringKey.foregroundColor: drawColor] as [NSAttributedStringKey : Any]
+        let myAttributedString = NSAttributedString(string: text, attributes: myAttributes)
+        a.string = myAttributedString
+        layer.addSublayer(a)
+    }
 }
